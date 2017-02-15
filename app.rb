541c30ac51bc42c291da05e210bb8210a2b6aeaf
subbat9710 <tula.subba1991@gmail.com>
2017-02-15 14:50:47 -0500

    apps

--- app.rb
+++ app.rb
@@ -2,13 +2,18 @@ require 'sinatra'
 require_relative "isbn.rb"
 
 get '/' do 
-
+   Answer = ""
    erb :isbn_input
 end
 
 post '/isbnnum' do 
-	#num = params['ISBN']
 	num = params[:ISBN]
 	pgm_results = validate_input10(num)
-	"Answer is #{ pgm_results}"
+	#{}"Isbn number is #{ pgm_results}" #it shows the results
+    if pgm_results==true
+    Answer = "Valid ISBN is here#{num}"
+    else
+    Answer = "Invalid ISBN is here#{num}"
+    end
+  erb :isbn_input 
 end
